1dafb96d27edb86dfd6ae6eed6f99e95c1c364ba
Omar Mason <mr.omarmason@gmail.com>
2017-04-28 00:13:16 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,42 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures=Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    @titles=Title.all
+    @landmarks=Landmark.all
+    erb :'figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  post '/figures/create' do
+     @figure = Figure.create(params[:figure])
+     if !params[:title][:name].empty?
+       @figure.titles << Title.create(params[:title])
+     end
+
+     if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+    redirect to "/figures/#{@figure.id}"
+  end
+
+  get '/figures/:id/edit' do
+    @titles=Title.all
+    @landmarks=Landmark.all
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+
+
+
+end
